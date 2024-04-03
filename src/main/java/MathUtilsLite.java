39da4c9a2df0869823e38d5690c50dff9b8b930d
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-04-03 12:59:48 +0200

    Intitial commit

---
+++ src/main/java/MathUtilsLite.java
@@ -0,0 +1,60 @@
+public class MathUtilsLite {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+	public double pi() {
+		return Math.PI;
+	}
+
+	public double power(double a, double b) {
+		return Math.pow(a, b);
+	}
+
+	public double sqrt(double a) {
+		return Math.sqrt(a);
+	}
+
+	public double log(double a) {
+		return Math.log(a);
+	}
+
+	public double sin(double a) {
+		return Math.sin(a);
+	}
+
+	public double cos(double a) {
+		return Math.cos(a);
+	}
+
+	public double tan(double a) {
+		return Math.tan(a);
+	}
+
+	public double asin(double a) {
+		return Math.asin(a);
+	}
+
+	public double acos(double a) {
+		return Math.acos(a);
+	}
+
+	public double atan(double a) {
+		return Math.atan(a);
+	}
+
+
+}
